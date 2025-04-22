b1640e4735309cad83053e1d88d4bb813f8b2766
Deliu Marius <marius.profesional@gmail.com>
2025-04-22 18:23:17 +0300

    Prep for rewriting.

--- include/constants.h
+++ include/constants.h
@@ -6,10 +6,15 @@
 #define ENTER   10
 #define ESC     27
 
-#define APP_SIDE_WIDTH      (strlen(__APP_BANNER__[0])+4)
-#define APP_BANNER_HEIGHT   9
-#define APP_BANNER_LINES    5
 
-extern char* __APP_BANNER__[APP_BANNER_LINES];
+#define APP_SIDE_WIDTH              (strlen(__APP_BANNER__[0])+4)
+#define APP_BANNER_HEIGHT           9
+#define APP_BANNER_LINES            5
+#define APP_MAIN_MENU_CHOICES_NO    6
+
+
+extern const char* __APP_BANNER__[APP_BANNER_LINES];
+extern const char* __APP_MAIN_MENU_CHOICES[APP_MAIN_MENU_CHOICES_NO];
+
 
 #endif
--- src/constants.c
+++ src/constants.c
@@ -1,9 +1,19 @@
 #include "constants.h"
 
-char* __APP_BANNER__[] = {
+const char* __APP_BANNER__[] = {
     "       _____                                        ___       ",
     "      / __(_)___  ____ _____  ________        _____/ (_)      ",
     "     / /_/ / __ \\/ __ `/ __ \\/ ___/ _ \\______/ ___/ / /    ",
     "    / __/ / / / / /_/ / / / / /__/  __/_____/ /__/ / /        ",
     "   /_/ /_/_/ /_/\\__,_/_/ /_/\\___/\\___/      \\___/_/_/     "
+};
+
+const char* __APP_MAIN_MENU_CHOICES[] = { 
+    " Accounts ",
+    " Transactions ",
+    " Currencies ",
+    " Transaction Categories ",
+    " Options ",
+    
+    " Exit "
 };
